fdad9fea24f78f84c7d5974514be930371c76632
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-10-01 05:27:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,4 +7,12 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + "!" }
 end
+  
+def long_planeteer_calls(array)
+  # Your code here
+end
 
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
