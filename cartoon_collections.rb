a56d64a6e8537f6e7ed1555e275e2094f0de8775
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-10-01 05:23:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,3 +4,20 @@ def roll_call_dwarves(array)
 end
 end
 
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize
+    puts 
+  end
+end
+  
+end
+
+def long_planeteer_calls# code an argument here
+  # Your code here
+end
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
