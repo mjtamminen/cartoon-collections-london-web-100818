892d6bdacd36cb19a2abf9bbeb7344d4c76a561c
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-10-01 05:35:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese(cheeses)
+def find_the_cheese(array)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
