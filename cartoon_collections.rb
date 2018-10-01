582b9a55b934cac854753c7cfa613471666d738a
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-10-01 05:19:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,13 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + "!" }
+end
+  
 end
 
 def long_planeteer_calls# code an argument here
